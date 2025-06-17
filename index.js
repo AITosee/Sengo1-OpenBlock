d6daca30ab1265cd9d083dbe4e30b9e186da19b2
feng <succeed_happu@outlook.com>
2025-06-18 07:59:52 +0800

    refactor(sengo1): 更新 Sengo1 视觉扩展的名称和版本信息
    - 将扩展名称和 ID 统一为 'Sengo1'
    - 更新版本号至 0.0.4
    - 修改标签为 'AI'
    - 更新发布工作流以支持 KidsBlock 文件的创建和打包

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const sengo1 = formatMessage => ({
     name: formatMessage({
-        id: 'sengo1.name',
+        id: 'Sengo1.name',
         default: 'Sengo1',
         description: 'name of Sengo1 vision sensor extension'
     }),
-    extensionId: 'sengo1',
-    version: '0.0.3',
+    extensionId: 'Sengo1',
+    version: '0.0.4',
     supportDevice: ['arduinoUno','arduinoUnoR4Wifi','arduinoUnoR4Minima', 'arduinoNano', 'arduinoLeonardo', 'arduinoMega2560', 
         'arduinoEsp32', 'arduinoEsp8266','arduinoEsp32S3',
         'arduinoK210','arduinoK210MaixDock','arduinoK210Maixduino','arduinoLeonardo',
@@ -13,7 +13,7 @@ const sengo1 = formatMessage => ({
     author: 'AiToose',
     iconURL: `assets/sengo1.png`,
     description: formatMessage({
-        id: 'sengo1.description',
+        id: 'Sengo1.description',
         default: 'Sengo1 vision sensor extension for OpenBlock.',
         description: 'Description of Sengo1 vision sensor'
     }),
@@ -21,10 +21,11 @@ const sengo1 = formatMessage => ({
     blocks: 'blocks.js',
     generator: 'generator.js',
     toolbox: 'toolbox.js',
+    msg: 'translations.js',
     translations: 'translations.js',
     library: 'lib',
-    tags: ['sensor', 'vision'],
+    tags: ['sensor', 'AI'],
     helpLink: 'https://tosee.readthedocs.io/zh/latest/'
 });
 
-module.exports = sengo1; 
+module.exports = Sengo1; 
